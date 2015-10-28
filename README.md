079f870c257fe05b3cd5b742a66eab9ce1280b6b
Ryan Loader <ryan.loader@oneleggedcrab.com>
2015-10-29 11:06:35 +1300

    Initial commit

---
+++ README.md
@@ -0,0 +1,107 @@
+# GMDoc
+
+***The missing documentation tool for GameMaker Extensions.***
+
+Generates documentation files in HTML format from a given GameMaker project. The tool allows implicit documentation simply
+by adding comments to your scripts and objects. You can document your scripts as follows;
+
+```
+/// my_cool_script(arg0,arg1)
+/// Simply adds the two arguments together.
+/// @param arg0 The first argument to the function
+/// @param arg1 The second argument to the function
+/// @return An awesome value that you probably want to deal with
+
+return argument0 + argument1
+```
+
+Each script has four components;
+
+- *Usage* eg. my_cool_script(arg0,arg1)
+- *Description* eg. Simply adds the two arguments together.
+- *Parameters* in the format <name> <description> eg. arg0 The first argument to the function
+- *Return Statement* eg. An awesome value that you probably want to deal with
+
+You can comment any way you like, the tool will stop reading your comments once a break in
+leading comment is found.
+
+```
+/*
+** another_cool_script()
+** I prefer stars in my docs
+** @return Another value
+*/
+
+return 5;
+```
+
+You can also use markup in your comments to give headings, bold text and code examples like so;
+
+```
+/// my_super_well_documented_function()
+/// <p>This is an awesome function that doesn't do very much, but has 
+/// awesome documentation.</p>
+/// <p>We can add some extra spacing using line breaks and paragraph tags.<p>
+///	</br>
+/// <h5>Example</h5>
+/// <p>Here is an example of how to use this function.</p>
+/// <pre>
+/// var greeting = my_super_well_documented_function();
+/// show_debug_message(greeting);
+/// </pre>
+/// <b>This will be bold</b>
+
+return "Hello World";
+```
+
+Documentation like this is useful because it easy to do and your users can simply look at your
+scripts to get all the information that's also available in the API reference. It's about not
+having to make documentation a separate, painstatking task, it should all just come together.
+
+### Installation
+
+You must have [python 2.x](https://www.python.org/downloads/) installed to use this library. Follow the instructions on [python.org](https://www.python.org/) for installation on your specific platform.
+
+I plan to add binaries for the library in the future, for now it should suffice to simply clone this repo.
+
+### Usage
+
+Command Line
+
+```
+python gmdoc.py /path/to/MyProject.gmx/MyProject.project.gmx
+```
+
+### TODO 
+
+- Document extension constants
+- Add type information to parameters and returns
+- Document objects and their properties
+- Allow additional docs through game information
+
+### License
+
+> This is free and unencumbered software released into the public domain.
+
+> Anyone is free to copy, modify, publish, use, compile, sell, or
+distribute this software, either in source code form or as a compiled
+binary, for any purpose, commercial or non-commercial, and by any
+means.
+
+> In jurisdictions that recognize copyright laws, the author or authors
+of this software dedicate any and all copyright interest in the
+software to the public domain. We make this dedication for the benefit
+of the public at large and to the detriment of our heirs and
+successors. We intend this dedication to be an overt act of
+relinquishment in perpetuity of all present and future rights to this
+software under copyright law.
+
+> THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+> For more information, please refer to <http://unlicense.org/>
---
+++ gmdoc.py
@@ -0,0 +1,167 @@
+import sys, os, shutil, xml, re
+import xml.etree.ElementTree as ET
+from jinja2 import Environment, PackageLoader
+JINJA_ENV = Environment(loader=PackageLoader('gmdoc', 'templates'))
+
+# Templates
+METHOD_TEMPLATE = JINJA_ENV.get_template('layout.html')
+
+# Basic types
+TYPE_REAL = 'real'
+TYPE_STRING = 'string'
+
+# Represents a method in the source code
+class Method():
+    def __init__(self, name, syntax):
+    	self.name = name
+        self.params = []
+        self.syntax = syntax
+        self.description = ''
+
+# Represents a method parameter in the source code
+class Param():
+	def __init__(self, name, type, desc):
+		self.name = name
+		self.type = type
+		self.description = desc
+
+# Kills the program and prints the given message
+def die(message):
+	print(message)
+	sys.exit()
+
+# Print the intended usage of the program
+def usage():
+	die("usage: "+sys.argv[0]+" <target.project.gmx>")
+
+# Reads an xml file and returns the root node
+def read_xml(the_dir):
+	the_file = open(the_dir,"r")
+	contents = the_file.read()
+	the_file.close()
+	return ET.fromstring(contents)
+
+# Extracts all comments from the given code
+def extract_comment_text(code):
+	state = 0
+	last = ''
+	result = ''
+	for char in code:
+		# Once we have read the entire first comment
+		# section and we hit code, we are done. Return
+		# the result
+		if state == -1:
+			if char == '/' or char == '*':
+				state = 0
+			elif char != ' ':
+				return result
+
+		if state == 0:
+			if char == '\n':
+				state = -1
+			elif last == '/':
+				if char == '*':
+					result += last + char
+					state = 2
+				elif char == '/':
+					result += last + char
+					state = 1
+		elif state == 1:
+			if char == '\n':
+				state = 0
+			result += char
+		elif state == 2:
+			if last == '*' and char == '/':
+				state = 0
+			result += char
+		last = char
+	return result
+
+# Removes any / * and spaces from the start of the given string
+def strip_leading_comment_markup(code):
+	i = 0
+	for char in code:
+		if char != ' ' and char != '/' and char != '*':
+			return code[i:]
+		else:
+			i += 1
+	return ''
+
+# 
+def strip_token(token, code):
+	i = code.find(token)
+	if i >= 0:
+		return code[i+len(token):].strip()
+
+
+# Documents the given project file
+def doc(project_file, outdir):
+	# Read the contents of the project file
+	print("Reading project file...");
+	project_dir = os.path.dirname(project_file)
+	project_xml = read_xml(project_file)
+	print(">> Successfully read project file");
+	# Initialize all the project things
+	project_methods = []
+	# Read all the project scripts and extract
+	# doc information from them
+	print("Reading scripts...")
+	for script in project_xml.iter("script"):
+		script_path = script.text.replace("\\","/")
+		script_path = os.path.join(project_dir,script_path)
+		script_name_and_ext = os.path.basename(script_path)
+		script_name = os.path.splitext(script_name_and_ext)[0]
+		# Skip scripts that start with an underscore
+		if script_name[0] == '_': 
+			print('Skipping private script ' + script_name)
+			continue
+		else:
+			print("Reading script " + script_name + "...")
+		# Read the script file
+		script_file = open(script_path,"r")
+		code = script_file.read()
+		script_file.close()
+		comments = extract_comment_text(code).split('\n')
+		syntax = strip_leading_comment_markup(comments[0])
+		comments = comments[1:]
+		# Generate a method object from the script file
+		method = Method(script_name, syntax)
+		desc = ''
+		for comment in comments:
+			the_param = strip_token("@param", comment)
+			the_return = strip_token("@return", comment)
+			if the_param:
+				the_param = the_param.split(' ',1)
+				method.params.append(Param(the_param[0], TYPE_REAL, the_param[1]))
+			elif the_return:
+				method.return_value = the_return
+			else:
+				desc += strip_leading_comment_markup(comment)
+		method.description = desc
+		print(method.description)
+		project_methods.append(method)
+	print(">> Successfully read scripts")
+
+	# Write the HTML files
+	if not os.path.exists(outdir):
+		os.makedirs(outdir)
+	for method in project_methods:
+		print("Rendering file %s", method.name)
+		the_html = METHOD_TEMPLATE.render(method=method, all_methods=project_methods)
+		text_file = open(os.path.join(outdir,method.name + '.html'), "w")
+		text_file.write(the_html)
+		text_file.close()
+
+# Check command line args
+if len(sys.argv) < 2:
+	print("Must supply a project file")
+	usage()
+
+# Set and validate the target project file
+TARGET_FILE = sys.argv[1]
+print("Project target: "+TARGET_FILE)
+if not os.path.isfile(TARGET_FILE):
+	die("Project file doesn't exist!")
+
+# Start documenting the given file
+doc(TARGET_FILE, './output')
